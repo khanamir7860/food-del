da4af4377fad66c5c761a1606d331b4b9e3c3aec
khanamir7860 <113623887+khanamir7860@users.noreply.github.com>
2024-09-17 11:28:19 +0530

    Update userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,7 @@
 import userModel from "../models/userModel.js";
 
 import jwt from "jsonwebtoken";
-const bcrypt = require("bcryptjs");
+import bcrypt from "bcryptjs";
 
 import validator from "validator";
 
